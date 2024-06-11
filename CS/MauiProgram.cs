9630344dbe1733260e8b4b79264b4051e6ff86da
Pavel Kravchenko <pavel.kravchenko@devexpress.com>
2024-06-11 15:08:24 +0400

    remove uneccessary classes

--- CS/MauiProgram.cs
+++ CS/MauiProgram.cs
@@ -1,31 +1,23 @@
 ﻿using DevExpress.Drawing.Internal;
 using DevExpress.Maui;
 using Microsoft.Extensions.Logging;
-using SkiaSharp;
-using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace FillPDFFile;
 
-public static class MauiProgram
-{
-    public static MauiApp CreateMauiApp()
-    {
-        DXDrawingEngine.ForceSkia();
-
+public static class MauiProgram {
+    public static MauiApp CreateMauiApp() {
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
             .UseDevExpress()
-            .ConfigureFonts(fonts =>
-            {
+            .ConfigureFonts(fonts => {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            })
-            .UseSkiaSharp();
+            });
 
-//#if DEBUG
-//        builder.Logging.AddDebug();
-//#endif
+        //#if DEBUG
+        //        builder.Logging.AddDebug();
+        //#endif
 
         return builder.Build();
     }
