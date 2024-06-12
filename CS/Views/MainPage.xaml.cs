ffc1d328b8aab9bc71a3da2694c252247bfa1463
Serge Pilipchuk <112853396+sergepilipchuk@users.noreply.github.com>
2024-06-12 19:05:59 +0400

    Refactor code (AcroForms)

--- CS/Views/MainPage.xaml.cs
+++ CS/Views/MainPage.xaml.cs
@@ -1,29 +1,10 @@
-﻿using DevExpress.Drawing;
-using FillPDFFile.Model;
-using DevExpress.Office.DigitalSignatures;
-using DevExpress.Pdf;
-using SkiaSharp;
-using SkiaSharp.Views.Maui.Controls;
-using DevExpress.Maui.DataForm;
-using DevExpress.Maui.Controls;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using Microsoft.Maui.Controls;
-using System.Reflection.Metadata;
-using FillPDFFile.ViewModels;
+﻿using Microsoft.Maui.Controls;
 
 namespace FillPDFFile;
 
-public partial class MainPage : ContentPage
-{
-    public MainPage()
-    {
+public partial class MainPage : ContentPage {
+    public MainPage() {
         InitializeComponent();
-    }
-
-    private void NavigatedToPage(object sender, NavigatedToEventArgs args)
-    {
-        ((MainPageViewModel)BindingContext).UpdatePreview();
     }
 }
 
